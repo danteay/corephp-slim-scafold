72f8e77a13d11ed38d7ee7ebcbbba80930fc3155
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2018-09-04 18:53:42 -0500

    Autoloading of route files
    Autoload route files from the **src/config/routes** folder

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__ . '/config/routes/ping.php';
+define('ROUTESDIR', __DIR__ . '/config/routes');
+
+$files = scandir(ROUTESDIR);
+
+foreach($files as $file) {
+    if (preg_match('/\.php$/', $file) && is_file(ROUTESDIR . "/{$file}")) {
+        require ROUTESDIR . "/{$file}";
+    }
+}
